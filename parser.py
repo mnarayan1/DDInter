daa6a47743e3a5bbc460200460af5e251594e73c
mnarayan1 <madhumitas.narayan@gmail.com>
2022-06-13 04:52:01 -0700

    fixes

--- parser.py
+++ parser.py
@@ -4,21 +4,17 @@ import os
 
 
 def load_data(data_folder):
+    csv_files = os.path.join(data_folder, "ddinter_downloads_code_*.csv")
+    joined_csv_files = glob.glob(csv_files)
+    merged_csv = pd.concat(
+        map(pd.read_csv, joined_csv_files), ignore_index=True)
 
-    # merging the files
-    joined_files = os.path.join(data_folder, "ddinter_downloads_code_*.csv")
-
-    # A list of all joined files is returned
-    joined_list = glob.glob(joined_files)
-
-    df = pd.concat(map(pd.read_csv, joined_list), ignore_index=True)
-
-    for index in df.index:
-        DDInterID_A = df['DDInterID_A'][index]
-        Drug_A = df['Drug_A'][index]
-        DDInterID_B = df['DDInterID_B'][index]
-        Drug_B = df['Drug_B'][index]
-        Level = df['Level'][index]
+    for index in merged_csv.index:
+        DDInterID_A = merged_csv['DDInterID_A'][index]
+        Drug_A = merged_csv['Drug_A'][index]
+        DDInterID_B = merged_csv['DDInterID_B'][index]
+        Drug_B = merged_csv['Drug_B'][index]
+        Level = merged_csv['Level'][index]
 
         doc = {}
         doc['_id'] = DDInterID_A+'_'+DDInterID_B+'_'+Level
