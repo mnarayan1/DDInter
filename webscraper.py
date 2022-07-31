9f49e49d686fcf8872d0a579e246dd4324f68e94
mnarayan1 <madhumitas.narayan@gmail.com>
2022-07-31 16:50:05 -0700

    scrape drug information from website

---
+++ webscraper.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import json
+
+
+def getDrugInfo():
+    for i in range(1, 1973):
+        URL = f"http://ddinter.scbdd.com/ddinter/drug-detail/DDInter{i}/"
+
+        # get html
+        page = urlopen(URL)
+        html = page.read().decode('utf-8')
+        soup = BeautifulSoup(html, 'html.parser')
+
+        links = soup.find_all('a')
+
+        drug_bank_id = ""
+        chembl_id = ""
+        pubchem_id = ""
+
+        for link in links:
+            link_content = link['href']
+            if 'chembl' in link_content:
+                chembl_id = link_content.split(
+                    'https://www.ebi.ac.uk/chembl/compound_report_card/')[-1]
+            if 'drugbank' in link_content:
+                drug_bank_id = link_content.split(
+                    'https://go.drugbank.com/drugs/')[-1]
+            if 'pubchem' in link_content:
+                pubchem_id = link_content.split(
+                    'https://pubchem.ncbi.nlm.nih.gov/compound/')[-1]
+
+        drug_info = {
+            'ddinter': i,
+            'drugbank': drug_bank_id,
+            'chembl': chembl_id,
+            'pubchem': pubchem_id
+        }
+        yield drug_info
+
+
+drug_info = getDrugInfo()
+
+data = {'records': []}
+
+for drug in drug_info:
+    data['records'].append(drug_info)
+
+with open('drug_data.json', 'w') as f:
+    json.dump(data, f)
