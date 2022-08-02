62a2409f673529a5d0b37a474816f075ffee62db
mnarayan1 <madhumitas.narayan@gmail.com>
2022-08-02 11:41:47 -0700

    ddinter url is not hardcoded

--- webscraper.py
+++ webscraper.py
@@ -2,41 +2,49 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import json
 
+import requests
+
 
 def getDrugInfo():
-    for i in range(1, 1973):
-        URL = f"http://ddinter.scbdd.com/ddinter/drug-detail/DDInter{i}/"
+    ddinter = 1
+    url_exists = True
+    while url_exists:
+        URL = f"http://ddinter.scbdd.com/ddinter/drug-detail/DDInter{ddinter}/"
+        if requests.get(URL).status_code == 200:
+            # get html
+            page = urlopen(URL)
+            html = page.read().decode('utf-8')
+            soup = BeautifulSoup(html, 'html.parser')
 
-        # get html
-        page = urlopen(URL)
-        html = page.read().decode('utf-8')
-        soup = BeautifulSoup(html, 'html.parser')
+            links = soup.find_all('a')
 
-        links = soup.find_all('a')
+            drug_bank_id = ""
+            chembl_id = ""
+            pubchem_id = ""
 
-        drug_bank_id = ""
-        chembl_id = ""
-        pubchem_id = ""
+            for link in links:
+                link_content = link['href']
+                if 'chembl' in link_content:
+                    chembl_id = link_content.split(
+                        'https://www.ebi.ac.uk/chembl/compound_report_card/')[-1]
+                if 'drugbank' in link_content:
+                    drug_bank_id = link_content.split(
+                        'https://go.drugbank.com/drugs/')[-1]
+                if 'pubchem' in link_content:
+                    pubchem_id = link_content.split(
+                        'https://pubchem.ncbi.nlm.nih.gov/compound/')[-1]
 
-        for link in links:
-            link_content = link['href']
-            if 'chembl' in link_content:
-                chembl_id = link_content.split(
-                    'https://www.ebi.ac.uk/chembl/compound_report_card/')[-1]
-            if 'drugbank' in link_content:
-                drug_bank_id = link_content.split(
-                    'https://go.drugbank.com/drugs/')[-1]
-            if 'pubchem' in link_content:
-                pubchem_id = link_content.split(
-                    'https://pubchem.ncbi.nlm.nih.gov/compound/')[-1]
-
-        drug_info = {
-            'ddinter': i,
-            'drugbank': drug_bank_id,
-            'chembl': chembl_id,
-            'pubchem': pubchem_id
-        }
-        yield drug_info
+            drug_info = {
+                'ddinter': ddinter,
+                'drugbank': drug_bank_id,
+                'chembl': chembl_id,
+                'pubchem': pubchem_id
+            }
+            ddinter += 1
+            yield drug_info
+        else:
+            url_exists = False
+            break
 
 
 drug_info = getDrugInfo()
@@ -44,7 +52,8 @@ drug_info = getDrugInfo()
 data = {'records': []}
 
 for drug in drug_info:
-    data['records'].append(drug_info)
+    data['records'].append(drug)
+    print(drug)
 
 with open('drug_data.json', 'w') as f:
     json.dump(data, f)
